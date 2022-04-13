ea5c42024b748cdc2cc2cea7f2918fd12208d685
Csandrade1 <andrade.cynthia07@gmail.com>
2022-04-12 23:48:49 -0700

    still working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+
+//mouseover
+const navLinks = document.querySelector(".nav");
+
+navLinks.addEventListener("mouseover", function (event) {
+  event.target.style.color = "red";
+});
+
+//dbl click
+const busImg = document.querySelector(".intro img");
+console.log(busImg);
+busImg.addEventListener("dblclick", function (e) {
+  alert("Hello World!!!");
+});
+
+//mouseleave
+const welcome = document.querySelector(".intro h2");
+
+welcome.addEventListener("mouseenter", function (event) {
+  event.target.style.color = "black";
+});
